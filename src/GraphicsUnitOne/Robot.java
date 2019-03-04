3d2ec6646b486d1c8aa8fb1ca689c9b1a9b2f678
rebekah <33032837+imrebsw@users.noreply.github.com>
2019-03-04 02:11:08 -0600

    Update Robot.java

--- src/GraphicsUnitOne/Robot.java
+++ src/GraphicsUnitOne/Robot.java
@@ -28,32 +28,52 @@ class Robot extends Canvas
       window.drawString("Robot LAB ", 35, 35 );
 
       //call head method
-      
-      //call other methods
-      
+       head(window);
+
+      //call other methods 
+   upperBody(window);
+   lowerBody(window);
+
    }
 
    public void head( Graphics window )
    {
-      window.setColor(Color.YELLOW);
+      window.setColor(Color.GREY);
+   // head
+   window.drawOval(300, 100, 200, 125);
+  
+   // eyes
+   window.setColor(Color.BLACK);
+   window.fillOval( 350, 155, 20, 20 );
+   window.fillOval( 430, 155, 20, 20 );
 
-      window.fillOval(300, 100, 200, 100);
-
-
-		//add more code here
-				
+   // mouth
+   window.drawArc(355, 150, 90, 20, 180, 180);			
    }
 
    public void upperBody( Graphics window )
    {
 
 		//add more code here
-   }
+   // body
+   window.setColor(Color.GREY);
+   window.drawOval(300, 225, 200, 225);
+   // arms
+   window.setColor(Color.BLACK);
+   window.drawRect(250, 250, 50, 125);
+   window.drawRect(500, 250, 50, 125);
+
+}
 
    public void lowerBody( Graphics window )
    {
 
-		//add more code here
+		//add more code here	
+   // legs
+   window.setColor(Color.BLACK);
+   window.drawRect(335, 450, 50, 75);
+   window.drawRect(415, 450, 50, 75);
+
 
    }
 }
