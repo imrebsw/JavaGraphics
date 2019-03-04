979932b5771b013ef7a71b545dee44624774899d
rebekah <33032837+imrebsw@users.noreply.github.com>
2019-03-04 02:19:24 -0600

    Update BigHouse.java

--- src/GraphicsUnitOne/BigHouse.java
+++ src/GraphicsUnitOne/BigHouse.java
@@ -36,6 +36,20 @@ public class BigHouse extends Canvas
       window.setColor(Color.BLUE);
 
       window.fillRect( 200, 200, 400, 400 );
+       protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            Graphics2D g2d = (Graphics2D) g.create();
+            int width = getWidth();
+            int height = getHeight();
+
+            g2d.setColor(new Color(139, 69, 19));
+            g2d.fillRect((width / 2) - 20, height / 2, 40, height / 2);
+
+            g2d.setColor(Color.GREEN);
+            int radius = 60;
+            g2d.fillOval((width / 2) - radius, (height / 2) - (radius), radius * 2, radius * 2);
+            g2d.dispose();
+        }
 
    }
 }
